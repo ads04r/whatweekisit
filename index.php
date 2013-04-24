9d338707fa0a8e805cfd2d03eb246aa7714d465e
ads04r <ads04r@ecs.soton.ac.uk>
2013-04-24 13:22:55 +0100

    Made it HTML instead of plain text!

--- index.php
+++ index.php
@@ -5,4 +5,11 @@ include("/var/wwwsites/phplib/academicyear.php");
 $start = getAcademicYearStart(time());
 $week = getCurrentWeek();
 
-print("It's week " . $week . " of the year that started " . date("jS F Y", $start));
+?><html>
+<head>
+<title>What week is it?</title>
+</head>
+<body>
+<p>It's week <? print($week); ?> of the year that started <? print(date("jS F Y", $start)); ?>.</p>
+</body>
+</html>
